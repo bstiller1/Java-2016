a831bd2dd245f8f98615f39d01037f1fe5fd6d95
Blake Stiller <stiller.blake@gmail.com>
2016-07-28 11:56:21 -0400

    Java 2016
    Only commit of the day
    GUI JavaFx
    7-28-16

---
+++ Welcome/src/WriteDataWithAutoClose.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WriteDataWithAutoClose {
+
+	public static void main(String[] args) throws IOException{
+		File file = new File("scores3.txt");
+		if(file.exists()){
+			System.out.println("File already exists");
+			System.exit(0);
+		}
+		try(PrintWriter output = new PrintWriter(file)){
+		output.print("John T Smith ");
+		output.println(90);
+		output.print("Eric K Jones ");
+		output.println(85);
+		}
+	}
+
+}
