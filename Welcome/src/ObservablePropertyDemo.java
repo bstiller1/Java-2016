4b48fa866ec79e43b51560e989a57bd0f46e13ae
Blake Stiller <stiller.blake@gmail.com>
2016-08-10 11:11:01 -0400

    Java 2016
    First Commit
    08-10-16

---
+++ Welcome/src/ObservablePropertyDemo.java
@@ -0,0 +1,15 @@
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class ObservablePropertyDemo {
+
+	public static void main(String[] args) {
+	DoubleProperty balance = new SimpleDoubleProperty();
+	balance.addListener(ov -> {
+			System.out.println("The new value is " + balance.doubleValue());
+	});
+	balance.set(4.5);
+	}
+
+}
